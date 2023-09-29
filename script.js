6a86358325b870aff1475b6c63fbb9b9871d45a9
Dgliump <lortkiphanidzeliaa@gmail.com>
2023-09-29 02:18:14 -0400

    mobile

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const countdown = () => {
-    const endDate = new Date("Sept 28, 2023 04:00:00").getTime();
+const count = () => {
+    const endDate = new Date("Sept 29, 2023 04:00:00").getTime();
     const now = new Date().getTime();
     const difference = endDate - now;
-
     const seconds = 1000;
     const minutes = seconds * 60;
     const hours = minutes * 60;
     const days = hours * 24;
+
     let timeDays = Math.floor(difference / days);
     let timeHours = Math.floor((difference % days) / hours);
     let timeMinutes = Math.floor((difference % hours) / minutes);
@@ -20,6 +20,14 @@ const countdown = () => {
     document.getElementById("hours").innerHTML = timeHours;
     document.getElementById("minutes").innerHTML = timeMinutes;
     document.getElementById("seconds").innerHTML = timeSeconds;
+
+    // if (timeDays == "00" && timeHours == "00" && timeMinutes == "00" && timeSeconds == "00"){
+    //     document.getElementById("days").innerHTML = "00";
+    //     document.getElementById("hours").innerHTML = "00";
+    //     document.getElementById("minutes").innerHTML = "00";
+    //     document.getElementById("seconds").innerHTML = "00";
+    // } 
+    // how????????????
 }
 
-setInterval(countdown, 1000);
+setInterval(count, 1000);
